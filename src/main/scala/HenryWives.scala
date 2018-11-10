3136c1121e033c5800ff177744c3f9c38882cddf
bettyES <betty.schirrmeister@gmail.com>
2018-11-10 14:19:38 +0000

    using match - case expressions

--- src/main/scala/HenryWives.scala
+++ src/main/scala/HenryWives.scala
@@ -1,6 +1,18 @@
 object HenryWives{
   def survived (name: String, marriage: Int):Boolean ={
-    (name == "Catherine" && marriage >=2) || (name == "Anne" && marriage <=1)
+    //(name == "Catherine" && marriage >=2) || (name == "Anne" && marriage <=1)
+
+            name match {
+              case "Catherine" => marriage match {
+                case 1 => false
+                case _ => true
+              }
+              case "Anne" => marriage match {
+                case 1 => true
+                case _ => false
+              }
+              case _ => false
+            }
 
 
   }
