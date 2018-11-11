e3cd5f716f8ef196dda89dfd8b25545e4ed04a02
bettyES <betty.schirrmeister@gmail.com>
2018-11-11 09:52:02 +0000

    using match - case expressions with if clause

--- src/main/scala/HenryWives.scala
+++ src/main/scala/HenryWives.scala
@@ -2,18 +2,22 @@ object HenryWives{
   def survived (name: String, marriage: Int):Boolean ={
     //(name == "Catherine" && marriage >=2) || (name == "Anne" && marriage <=1)
 
-            name match {
-              case "Catherine" => marriage match {
-                case 1 => false
-                case _ => true
-              }
-              case "Anne" => marriage match {
-                case 1 => true
-                case _ => false
-              }
+//            name match {
+//              case "Catherine" => marriage match {
+//                case 1 => false
+//                case _ => true
+//              }
+//              case "Anne" => marriage match {
+//                case 1 => true
+//                case _ => false
+//              }
+//              case _ => false
+//            }
+            name match{
+              case "Catherine" if marriage >= 2  => true
+              case "Anne" if marriage <= 1 => true
               case _ => false
             }
-
 
   }
 }
