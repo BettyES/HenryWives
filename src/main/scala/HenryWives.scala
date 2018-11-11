f54033d74807c0bc448519b7fc3051dddea8f208
bettyES <betty.schirrmeister@gmail.com>
2018-11-11 09:52:58 +0000

    using match - case expressions with boolean

--- src/main/scala/HenryWives.scala
+++ src/main/scala/HenryWives.scala
@@ -13,11 +13,19 @@ object HenryWives{
 //              }
 //              case _ => false
 //            }
-            name match{
-              case "Catherine" if marriage >= 2  => true
-              case "Anne" if marriage <= 1 => true
-              case _ => false
-            }
+//            name match{
+//              case "Catherine" if marriage >= 2  => true
+//              case "Anne" if marriage <= 1 => true
+//              case _ => false
+//            }
+
+    (name == "Catherine" , name =="Anne" , marriage <= 1, marriage >=2) match{
+      case (true, false, false, true) => true
+      case(false, true, true, false) => true
+      case(false, false, true, false) => false
+      case(true,false,true, false) => false
+      case(false,true, false, true)=> false
+    }
 
   }
 }
